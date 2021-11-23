a1729f6ca43fbf0b41c38473d41f0370805e6f92
Daan Zwarthoed <78368488+DaanZwarthoed@users.noreply.github.com>
2021-11-23 12:47:06 +0100

    bubbles a square on a click

--- src/components/Grid/Cell/Cell.js
+++ src/components/Grid/Cell/Cell.js
@@ -2,7 +2,69 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Cell.css";
 
-const Cell = () => <div className="Cell"></div>;
+let chosenGrowAmount = 5;
+let newNeighboursArray = [];
+
+const hex2rgb = (hex) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+function addElementOrNot(element, randomColor) {
+  if (element) {
+    if (element.style.backgroundColor) {
+      if (element.style.backgroundColor !== randomColor) {
+        newNeighboursArray.push(element);
+      }
+    } else {
+      newNeighboursArray.push(element);
+    }
+  }
+}
+
+function getNeighbours(target, randomColor) {
+  newNeighboursArray = [];
+  addElementOrNot(target.nextSibling, randomColor);
+  addElementOrNot(target.previousSibling, randomColor);
+  addElementOrNot(
+    target.parentElement.nextSibling.children[target.className.split(" ")[0]],
+    randomColor
+  );
+  addElementOrNot(
+    target.parentElement.previousSibling.children[
+      target.className.split(" ")[0]
+    ],
+    randomColor
+  );
+  return newNeighboursArray;
+}
+
+function clickEventStart(event) {
+  let neighbours = getNeighbours(event.target);
+  let nextNeighbours = [];
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  event.target.style.backgroundColor = randomColor;
+  for (let iGrowAmount = 0; iGrowAmount < chosenGrowAmount; iGrowAmount++) {
+    // eslint-disable-next-line no-loop-func
+    setTimeout(() => {
+      neighbours.forEach((element) => {
+        if (element) {
+          element.style.backgroundColor = randomColor;
+          nextNeighbours.push(...getNeighbours(element, hex2rgb(randomColor)));
+        }
+      });
+      console.log(nextNeighbours);
+      neighbours = nextNeighbours;
+      nextNeighbours = [];
+    }, 100 * iGrowAmount);
+  }
+}
+
+const Cell = ({ className }) => {
+  return <div className={`${className} Cell`} onClick={clickEventStart}></div>;
+};
 
 Cell.propTypes = {};
 
--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -2,15 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Grid.css";
 import Cell from "./Cell/Cell.js";
+let cellArray = [];
+let rowArray = [];
 
-let CellArray = [];
+for (let iWidth = 20; iWidth < window.innerWidth; iWidth = iWidth + 20) {
+  cellArray.push(
+    <Cell key={iWidth / 20 - 1} className={iWidth / 20 - 1}></Cell>
+  );
+}
 
-for (let i = 0; i < 500; i++) {
-  CellArray.push(<Cell></Cell>);
+for (let iHeight = 20; iHeight < window.innerHeight; iHeight = iHeight + 20) {
+  rowArray.push(
+    <div key={iHeight / 20 - 1} className={`${iHeight / 20 - 1} row`}>
+      {cellArray}
+    </div>
+  );
 }
 
 const Grid = () => {
-  return <div class="Grid">{CellArray}</div>;
+  return <div className="Grid">{rowArray}</div>;
 };
 
 Grid.propTypes = {};
